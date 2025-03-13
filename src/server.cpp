588e650485b3ead4a5f8984cd19f36ae0435ca1e
Leo Chapard <lchapard@z2r8p5.42lyon.fr>
2025-03-13 12:05:54 +0100

    1rst push

--- src/server.cpp
+++ src/server.cpp
@@ -50,12 +50,30 @@ int accept_connexion(int server_fd, sockaddr_in *servaddr)
 	int	my_socket;
 	int	addr_len = sizeof(servaddr);
 
+	//here servaddr is the connecting ip
 	if ((my_socket = accept(server_fd, (struct sockaddr *)servaddr, (socklen_t*)&addr_len))<0)
 	{
 		perror("In accept");
 		return(0); //exit ?
 	}
 	return (my_socket);
+}
+
+void	analyse_request(char buffer[BUFFER_SIZE])
+{
+	std::string request(buffer);
+	std::string first_line;
+	std::string requested_file;
+	size_t		filename_start;
+	size_t		filename_end;
+
+	first_line = request.substr(0, request.find('\n')); // doesn´t work if curl
+	filename_start = first_line.find_first_of(' ');
+	filename_end = first_line.find_last_of(' ');
+	requested_file = first_line.substr(filename_start, filename_end - filename_start);
+	printf("\033[34m------------------------\n");
+	printf("%s\n",requested_file.c_str() );
+	printf("------------------------\033[0m\n");
 }
 
 int main(int argc, char **argv)
@@ -66,12 +84,12 @@ int main(int argc, char **argv)
 	int	server_fd;
 	struct sockaddr_in servaddr;
 	t_fd_data s_data; // to set select
-
-	if (argc != 2)
-	{
-		std::cout << "Wrong number of arguments ! " << std::endl;
-		return (0);
-	}	
+	(void)argc;
+	// if (argc != 2)
+	// {
+	// 	std::cout << "Wrong number of arguments ! " << std::endl;
+	// 	return (0);
+	// }	
 
 	server_fd = initialize_socket(&servaddr, &s_data);
 	if (server_fd < 0)
@@ -82,7 +100,7 @@ int main(int argc, char **argv)
 	FD_ZERO(&s_data.saved_sockets);
 	FD_SET(server_fd, &s_data.saved_sockets);
 	while(42)
-	{	
+	{
 		printf("\n\033[31m++ Waiting for new connection ++\033[0m\n\n");
 		s_data.ready_sockets = s_data.saved_sockets;
 		if (select(FD_SETSIZE, &s_data.ready_sockets, NULL, NULL, NULL) < 0)
@@ -95,7 +113,7 @@ int main(int argc, char **argv)
 		{
 			if (FD_ISSET(i, &s_data.ready_sockets))
 			{
-				if (i == server_fd)
+				if (i == server_fd) // there is a new connection available on the server socket
 				{
 					my_socket = accept_connexion(server_fd, &servaddr);
 					FD_SET(my_socket, &s_data.saved_sockets); //add new connection to current set
@@ -113,9 +131,7 @@ int main(int argc, char **argv)
 			perror("Failed to read ! ");
 			return (0);
 		}
-		printf("------------------------\n");
-		printf("%s\n",buffer );
-		printf("------------------------\n");
+		analyse_request(buffer);
 		write(my_socket , mess , strlen(mess));
 		std::cout << "message sent from server !\n" << std::endl;
 		close(my_socket);
