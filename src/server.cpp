0da52383752b3bc14bfc0952734715dc35890d16
Benjamin Humeau <bhumeau@z2r8p4.42lyon.fr>
2025-03-13 12:00:40 +0100

    first push

--- src/server.cpp
+++ src/server.cpp
@@ -1,28 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   server.c                                           :+:      :+:    :+:   */
+/*   server.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lchapard <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: bhumeau <bhumeau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 09:57:04 by lchapard          #+#    #+#             */
-/*   Updated: 2025/02/25 09:57:05 by lchapard         ###   ########.fr       */
+/*   Updated: 2025/03/12 13:41:32 by bhumeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/webserv.hpp"
 
+#include <fstream>
+
 int	initialize_socket(sockaddr_in *servaddr, t_fd_data *socket_data)
 {
 	int	server_fd;
-
 
 	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
 	{
 		perror("cannot create socket"); 
 		return (-1); 
 	}
-	
+
 	bzero(servaddr, sizeof(*servaddr));
 	bzero(socket_data, sizeof(*socket_data));
 
@@ -39,7 +40,7 @@ int	initialize_socket(sockaddr_in *servaddr, t_fd_data *socket_data)
 	}
 	if (listen(server_fd, 10) < 0)
 	{
-		perror("Failed to listen ! ");
+		perror("Failed to lizlibsten ! ");
 		return (-1);
 	}
 	return (server_fd);
@@ -58,10 +59,91 @@ int accept_connexion(int server_fd, sockaddr_in *servaddr)
 	return (my_socket);
 }
 
+std::string find_in_config_file(std::string content, std::string variable_name)
+{
+	std::size_t i = 0;
+	i = content.find(variable_name);
+	if (i != content.rfind(variable_name))
+		return ("-1");
+	if (i == std::string::npos)
+		return ("unset");
+	i += variable_name.length();
+	std::string res;
+	while (content[i] && content[i] != ';')
+	{
+		res.push_back(content.at(i));
+		i++;
+	}
+	return (res);
+}
+
+bool	copy_variable_values(std::string content, std::string config_variables[], std::string	struct_values[])
+{
+	for (int i = 0; i < 7; i++)
+	{
+		struct_values[i] = find_in_config_file(content, config_variables[i]);
+		std::cout << config_variables[i] << " = " << struct_values[i] << std::endl;
+		if (struct_values[i] == "unset")
+		{
+			std::cout << config_variables[i] << " not set in config file!" << std::endl;
+			return (true);
+		}
+		if (struct_values[i] == "-1")
+		{
+			std::cout << config_variables[i] << " set more than once in config file!" << std::endl;
+			return (true);
+		}
+	}
+	return false;
+}
+
+std::string	copy_content(std::string filename)
+{
+	std::string content;
+	std::string line;
+	std::ifstream infile(filename.c_str());
+	if (!infile.is_open())
+	{
+		std::cout << "Error, failed to open filename!" << std::endl;
+		return ("NULL");
+	}
+	while(infile)
+	{
+		std::getline(infile, line);
+		if(!infile)
+			break;
+		content += line + '\n';
+		line.clear();
+	}
+	if (content.empty())
+	{
+		std::cout << "Config file is empty!" << std::endl;
+		return ("NULL");
+	}
+	return (content);
+}
+
+bool	parse_config(std::string filename, std::string config_variables[])
+{
+	std::string content = copy_content(filename);
+	if (content == "NULL")
+		return true;
+	std::string	struct_values[7] = {"unset", "unset", "unset", "unset", "unset", "unset", "unset"};
+	if (!copy_variable_values(content, config_variables, struct_values))
+		return true;
+	return false;
+}
+
 int main(int argc, char **argv)
 {
-
-	(void)argv;
+	if (argc != 2)
+	{
+		std::cout << "Please execute as ./Webserv \"config_file_name\"!" << std::endl;
+		return (1);
+	}
+	std::string config_variables[7] = {"listen", "host", "server_name", "error_page", "client_max_body_size", "root", "index"};
+	if (parse_config(argv[1], config_variables))
+		return (1);	
 	int my_socket;
 	int	server_fd;
 	struct sockaddr_in servaddr;
