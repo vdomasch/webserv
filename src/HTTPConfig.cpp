a2b687e1940d44c2dd62582b623df2decdd81aa9
Benjamin Humeau <bhumeau@z2r9p5.42lyon.fr>
2025-04-09 14:25:01 +0200

    fixed if condition for server name
    Signed-off-by: Benjamin Humeau <bhumeau@z2r9p5.42lyon.fr>

--- src/HTTPConfig.cpp
+++ src/HTTPConfig.cpp
@@ -63,6 +63,21 @@ bool HTTPConfig::is_location(std::string key)
 	return false;
 }
 
+static bool	is_server_name_already_used(std::map<std::string, ServerConfig> &server_list, ServerConfig &server_temp)
+{
+	if (server_list.find(server_temp.get_string_port_number() + static_cast<std::string>(":") + server_temp.get_server_name()) != server_list.end())
+	{
+		std::cerr << "Error: Server name already exists for this port" << std::endl;
+		return true;
+	}
+	else if (server_list[server_temp.get_string_port_number()].get_server_name() == server_temp.get_server_name())
+	{
+		std::cerr << "Error: Server name already exists for this port" << std::endl;
+		return true;
+	}
+	return false;
+}
+
 bool	HTTPConfig::parse_http()
 {
 	ServerConfig server_temp;
